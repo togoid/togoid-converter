81dab3545862a0c2e66c44da4c73da3520765a4d
tanaka <tanaka@info-lounge.jp>
2021-01-25 13:05:44 +0900

    Update layout

--- pages/index.js
+++ pages/index.js
@@ -110,6 +110,7 @@ const Home = () => {
   const [namespaceList, setNamespaceList] = useState([])
   const [selectedNamespace, setSelectedNamespace] = useState([])
   const [executed, setExecuted] = useState(false)
+  const [modalStatus, setModalStatus] = useState(true)
 
   useEffect(() => {
     setNamespaceList([])
@@ -303,20 +304,27 @@ const Home = () => {
                                   </span>
                                 </label>
                                 {selectedNamespace[i] === v.name ? <button onClick={() => executeQuery(v)} className="radio__three_dots"/> : null}
-                                <div className="button_pull_down__children">
-                                  <button className="button_pull_down__children__item">
-                                    <svg className="icon" viewBox="0 0 24 24">
-                                      <path fill="currentColor" d="M4,15V9H12V4.16L19.84,12L12,19.84V15H4Z" />
-                                    </svg>
-                                    Forward via this
-                                  </button>
-                                  <button className="button_pull_down__children__item">
-                                    <svg className="icon" viewBox="0 0 24 24">
-                                      <path fill="currentColor" d="M9,5V9H21V5M9,19H21V15H9M9,14H21V10H9M4,9H8V5H4M4,19H8V15H4M4,14H8V10H4V14Z" />
-                                    </svg>
-                                    Resolve with this
-                                  </button>
-                                </div>
+                                {selectedNamespace[i] === v.name ? (
+                                  <div className="button_pull_down__children">
+                                    <button
+                                      className="button_pull_down__children__item">
+                                      <svg className="icon" viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                              d="M4,15V9H12V4.16L19.84,12L12,19.84V15H4Z"/>
+                                      </svg>
+                                      Forward via this
+                                    </button>
+                                    <button
+                                      className="button_pull_down__children__item">
+                                      <svg className="icon" viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                              d="M9,5V9H21V5M9,19H21V15H9M9,14H21V10H9M4,9H8V5H4M4,19H8V15H4M4,14H8V10H4V14Z"/>
+                                      </svg>
+                                      Resolve with this
+                                    </button>
+                                  </div>
+                                ) : ("")
+                                }
                               </div>
                             </li>
                           })
@@ -331,6 +339,148 @@ const Home = () => {
                     })
                     : null
                   }
+
+                  {/* Modal */}
+                  {
+                    modalStatus ? (
+                      <div className="modal">
+                        <div className="modal__inner">
+                          <div className="modal__scroll_area">
+                            <button onClick={() => setModalStatus(!modalStatus)} className="modal__close">
+                              <svg viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+                              </svg>
+                            </button>
+                            <h2 className="title">ID forwarding</h2>
+                            <div className="modal__top">
+
+                              <div className="option">
+                                <p className="label">Option</p>
+                                <select name="" id="" className="select white">
+                                  <option value="id">ID</option>
+                                </select>
+                              </div>
+
+                              <div className="item_wrapper">
+                                <div className="input_search">
+                                  <svg className="input_search__icon" viewBox="0 0 24 24">
+                                    <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                                  </svg>
+                                  <input type="search" className="input_search__input"/>
+                                </div>
+
+                                <button className="button_icon">
+                                  <svg className="button_icon__icon" viewBox="0 0 24 24">
+                                    <path fill="currentColor" d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" />
+                                  </svg>
+                                  <span className="button_icon__label">エクスポート</span>
+                                </button>
+                              </div>
+
+                            </div>
+
+                            <table className="table">
+                              <thead>
+                              <tr>
+                                <th>NCBI Gene</th>
+                                <th>HGNC</th>
+                                <th>PATH</th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                              <tr>
+                                <td>Xxx-xxx-xxxx</td>
+                                <td>Zzz-zzz-zzzz</td>
+                                <td>
+                                  <ul className="buttons">
+                                    <li>
+                                      <button className="button_small green">NCBI Gene</button>
+                                    </li>
+                                    <li>
+                                      <button className="button_small white">rdfs:seeAlso</button>
+                                    </li>
+                                  </ul>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>Xxx-xxx-xxxx</td>
+                                <td>Zzz-zzz-zzzz</td>
+                                <td>
+                                  <ul className="buttons">
+                                    <li>
+                                      <button className="button_small green">NCBI Gene</button>
+                                    </li>
+                                    <li>
+                                      <button className="button_small white">rdfs:seeAlso</button>
+                                    </li>
+                                  </ul>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>Xxx-xxx-xxxx</td>
+                                <td>Zzz-zzz-zzzz</td>
+                                <td>
+                                  <ul className="buttons">
+                                    <li>
+                                      <button className="button_small green">NCBI Gene</button>
+                                    </li>
+                                    <li>
+                                      <button className="button_small white">rdfs:seeAlso</button>
+                                    </li>
+                                  </ul>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>Xxx-xxx-xxxx</td>
+                                <td>Zzz-zzz-zzzz</td>
+                                <td>
+                                  <ul className="buttons">
+                                    <li>
+                                      <button className="button_small green">NCBI Gene</button>
+                                    </li>
+                                    <li>
+                                      <button className="button_small white">rdfs:seeAlso</button>
+                                    </li>
+                                  </ul>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>Xxx-xxx-xxxx</td>
+                                <td>Zzz-zzz-zzzz</td>
+                                <td>
+                                  <ul className="buttons">
+                                    <li>
+                                      <button className="button_small green">NCBI Gene</button>
+                                    </li>
+                                    <li>
+                                      <button className="button_small white">rdfs:seeAlso</button>
+                                    </li>
+                                  </ul>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>Xxx-xxx-xxxx</td>
+                                <td>Zzz-zzz-zzzz</td>
+                                <td>
+                                  <ul className="buttons">
+                                    <li>
+                                      <button className="button_small green">NCBI Gene</button>
+                                    </li>
+                                    <li>
+                                      <button className="button_small white">rdfs:seeAlso</button>
+                                    </li>
+                                  </ul>
+                                </td>
+                              </tr>
+                              </tbody>
+                            </table>
+                            <button className="button_more">MORE</button>
+                          </div>
+                        </div>
+                      </div>
+                    ) : ("")
+                  }
+
                 </div>
               </div>
             </div>
