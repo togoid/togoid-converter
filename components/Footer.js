f83bea11c48529b14e1ecfc7a4583513362f7312
Souta Ito <soutaito@info-lounge.jp>
2021-03-07 16:08:43 +0900

    Minor text update

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Header = () => {
         <img src="/images/footer_logo.png" alt="" className="footer__logo"/>
         <p className="footer__text">Address: Univ. of Tokyo Kashiwa-no-ha Campus Station Satellite 6F. 178-4-4 Wakashiba, Kashiwa-shi, Chiba 277-0871, JAPAN</p>
         <p className="footer__text">Phone: +81 (4) 7135 5508</p>
-        <p className="footer__text">Copyright © 2019 Database Center for Life Science (DBCLS)</p>
+        <p className="footer__text">Copyright © Database Center for Life Science (DBCLS)</p>
       </div>
     </footer>
   )
