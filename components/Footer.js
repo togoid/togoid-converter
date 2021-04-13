cf2139a757c4961d6e4231e28e2b147b444525a8
Souta Ito <soutaito@info-lounge.jp>
2021-04-13 13:28:37 +0900

    Fix eslint errors

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,12 @@
-const Header = () => {
-  return (
-    <footer className="footer">
-      <div className="footer__inner">
-        <img src="/images/footer_logo.png" alt="" className="footer__logo"/>
-        <p className="footer__text">Address: Univ. of Tokyo Kashiwa-no-ha Campus Station Satellite 6F. 178-4-4 Wakashiba, Kashiwa-shi, Chiba 277-0871, JAPAN</p>
-        <p className="footer__text">Phone: +81 (4) 7135 5508</p>
-        <p className="footer__text">Copyright © Database Center for Life Science (DBCLS)</p>
-      </div>
-    </footer>
-  )
-}
+const Header = () => (
+  <footer className="footer">
+    <div className="footer__inner">
+      <img src="/images/footer_logo.png" alt="" className="footer__logo" />
+      <p className="footer__text">Address: Univ. of Tokyo Kashiwa-no-ha Campus Station Satellite 6F. 178-4-4 Wakashiba, Kashiwa-shi, Chiba 277-0871, JAPAN</p>
+      <p className="footer__text">Phone: +81 (4) 7135 5508</p>
+      <p className="footer__text">Copyright © Database Center for Life Science (DBCLS)</p>
+    </div>
+  </footer>
+);
 
-export default Header
+export default Header;
