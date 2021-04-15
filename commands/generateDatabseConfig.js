0526373d89202e8fc328f5f24b72640a34e9fe56
Souta Ito <soutaito@info-lounge.jp>
2021-04-15 11:04:46 +0900

    Update memo

--- commands/generateDatabseConfig.js
+++ commands/generateDatabseConfig.js
@@ -2,6 +2,41 @@ const fs = require("fs"),
   path = require("path");
 const dir = "../togoid-config/config/";
 
+const configs = {};
+
+/* 完成イメージ
+const configs = {
+  "affy_probeset-ncbigene": {
+    link: {
+      forward: {
+        label: "seeAlso",
+      },
+      reverse: {
+        label: "seeAlso",
+      },
+    },
+    update: {
+      frequency: "freeze",
+      method: "sparql_csv2tsv.sh query.rq https://orth.dbcls.jp/sparql-dev",
+    },
+  },
+  "chembl_compound-chebi": {
+    link: {
+      forward: {
+        label: "seeAlso",
+      },
+      reverse: {
+        label: "seeAlso",
+      },
+    },
+    update: {
+      frequency: "freeze",
+      method: "sparql_csv2tsv.sh query.rq https://orth.dbcls.jp/sparql-dev",
+    },
+  },
+};
+*/
+
 const walk = function (p, fileCallback, errCallback) {
   fs.readdir(p, function (err, files) {
     if (err) {
@@ -10,10 +45,11 @@ const walk = function (p, fileCallback, errCallback) {
     }
 
     files.forEach(function (f) {
-      var fp = path.join(p, f); // to full-path
+      const fp = path.join(p, f); // to full-path
       if (fs.statSync(fp).isDirectory()) {
         walk(fp, fileCallback); // ディレクトリなら再帰
       } else if (fs.statSync(fp).isFile() && /.*\.yaml/.test(fp)) {
+        // todo yamlを読み込んでJSのオブジェクトに変換し、configsに格納
         fileCallback(fp); // ファイルならコールバックで通知
       }
     });
@@ -30,3 +66,5 @@ walk(
     console.log("Receive err:" + err); // エラー受信
   }
 );
+
+// todo configsに蓄積したオブジェクトをJSONにして、public配下にconfig.jsonのファイル名で保存
