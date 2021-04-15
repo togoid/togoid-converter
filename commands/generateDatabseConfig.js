4024bfeac95c0f9ec847b9ef69879d2856a629be
Souta Ito <soutaito@info-lounge.jp>
2021-04-15 15:52:47 +0900

    Generate dataset.json

--- commands/generateDatabseConfig.js
+++ commands/generateDatabseConfig.js
@@ -1,42 +1,9 @@
 const fs = require("fs"),
   path = require("path"),
-  yaml = require('js-yaml');
+  yaml = require("js-yaml");
 const dir = "../togoid-config/config/";
 
 const configs = {};
-
-/* 完成イメージ
-const configs = {
-  "affy_probeset-ncbigene": {
-    link: {
-      forward: {
-        label: "seeAlso",
-      },
-      reverse: {
-        label: "seeAlso",
-      },
-    },
-    update: {
-      frequency: "freeze",
-      method: "sparql_csv2tsv.sh query.rq https://orth.dbcls.jp/sparql-dev",
-    },
-  },
-  "chembl_compound-chebi": {
-    link: {
-      forward: {
-        label: "seeAlso",
-      },
-      reverse: {
-        label: "seeAlso",
-      },
-    },
-    update: {
-      frequency: "freeze",
-      method: "sparql_csv2tsv.sh query.rq https://orth.dbcls.jp/sparql-dev",
-    },
-  },
-};
-*/
 
 const walk = function (p, fileCallback, errCallback) {
   fs.readdir(p, function (err, files) {
@@ -46,30 +13,30 @@ const walk = function (p, fileCallback, errCallback) {
     }
 
     files.forEach(function (f) {
-      if(f == 'dataset.yaml'){
-        return
-      }
       const fp = path.join(p, f); // to full-path
+      if (f === "dataset.yaml") {
+        fs.writeFileSync(
+          "../public/dataset.json",
+          JSON.stringify(yaml.load(fs.readFileSync(fp, "utf8")), null, 2)
+        );
+        return;
+      }
       if (fs.statSync(fp).isDirectory()) {
         walk(fp, fileCallback); // ディレクトリなら再帰
       } else if (fs.statSync(fp).isFile() && /.*\.yaml/.test(fp)) {
-        // todo yamlを読み込んでJSのオブジェクトに変換し、configsに格納
-        configs[path.basename(p)] = fileCallback(fp); // ファイルならコールバックで通知
+        configs[path.basename(p)] = fileCallback(fp);
       }
     });
   });
 };
 
-// 使う方
 walk(
   dir,
   function (filePath) {
-    // console.log(filePath); // ファイル１つ受信
-    try{
-      return yaml.load(fs.readFileSync(filePath,'utf8'))
-    }
-    catch (err){
-      console.error(err.message)
+    try {
+      return yaml.load(fs.readFileSync(filePath, "utf8"));
+    } catch (err) {
+      console.error(err.message);
     }
   },
   function (err) {
@@ -77,8 +44,6 @@ walk(
   }
 );
 
-setTimeout(function(){
-  fs.writeFileSync("../public/config.json", JSON.stringify(configs, null, 2))
-},5000)
-
-// todo configsに蓄積したオブジェクトをJSONにして、public配下にconfig.jsonのファイル名で保存
+setTimeout(function () {
+  fs.writeFileSync("../public/config.json", JSON.stringify(configs, null, 2));
+}, 5000);
