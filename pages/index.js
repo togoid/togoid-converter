392d1cc53e86f602d1a3128aba0bdbe28cb13b09
Souta Ito <soutaito@info-lounge.jp>
2021-04-20 17:54:37 +0900

    [WIP] refacotring

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ import Footer from "../components/Footer";
 import Explore from "../components/Explore";
 import Databases from "../components/Databases";
 import IdInput from "../components/IdInput";
-import { idPatterns } from "../lib/util";
 import axios from "axios";
+import dbConfig from "../public/config.json";
+import dbCatalogue from "../public/dataset.json";
 
 const Home = () => {
   const [ids, setIds] = useState([]);
@@ -14,10 +15,67 @@ const Home = () => {
   const [databaseNodes, setDatabaseNodes] = useState([]);
   const [route, setRoute] = useState([]);
 
-  useEffect(() => {
-    if (route.length > 1) {
-      executeQuery(route);
+  useEffect(async () => {
+    if (route.length > 0) {
+      const nodes = databaseNodes.slice(0, route.length);
+      const r = route[route.length - 1];
+      const candidates = [];
+      Object.keys(dbConfig).forEach((k) => {
+        if (k.indexOf(r.name) === 0) {
+          candidates.push({
+            name: k,
+            count: 1,
+            ids: [],
+            hasMenu: false,
+          });
+        }
+      });
+      // 先端の変換候補を追加
+      nodes[route.length] = candidates;
+      setDatabaseNodes(nodes);
     }
+    // if (route.length > 1) {
+    //   // TODO クエリ実行中にloading画面を表示させる
+    //   const r = [
+    //     route[route.length - 2].name.split("-").pop(),
+    //     route[route.length - 1].name.split("-").pop(),
+    //   ].join(",");
+    //   const ids = route[route.length - 2].ids.map((v) => v.to).join(",");
+    //   const d = await axios
+    //     .get(
+    //       `${process.env.NEXT_PUBLIC_SPARQL_ENDOPOINT}/convert?ids=${ids}&route=${r}`
+    //     )
+    //     .then((d) => d.data)
+    //     .catch((e) => console.log(e));
+    //
+    //   console.log(d);
+    //
+    //   const nodes = [...databaseNodes];
+    //   if (d) {
+    //     const candidates = [];
+    //     d.result.forEach((v) => {
+    //       const index = candidates.findIndex((pref) => pref.name === v.tn);
+    //       if (index === -1) {
+    //         candidates.push({
+    //           name: v.tn,
+    //           count: 1,
+    //           hasMenu: false,
+    //           ids: [{ from: v.f, to: v.t }],
+    //         });
+    //       } else {
+    //         candidates[index].count += 1;
+    //         candidates[index].ids.push({ from: v.f, to: v.t });
+    //       }
+    //     });
+    //     candidates.sort((a, b) => {
+    //       if (a.count < b.count) return 1;
+    //       if (a.count > b.count) return -1;
+    //       return 0;
+    //     });
+    //     nodes.push(candidates);
+    //     setDatabaseNodes(nodes);
+    //   }
+    // }
   }, [route]);
 
   /**
@@ -26,14 +84,14 @@ const Home = () => {
   const searchDatabase = (ids) => {
     const candidates = [];
     ids.forEach((id) => {
-      for (const key in idPatterns) {
-        if (id.match(idPatterns[key].regexp)) {
+      Object.keys(dbCatalogue).forEach((k) => {
+        if (id.match(dbCatalogue[k].regex)) {
           const index = candidates.findIndex(
-            (databases) => databases.name === key
+            (databases) => databases.name === k
           );
           if (index === -1) {
             candidates.push({
-              name: key,
+              name: k,
               count: 1,
               ids: [id],
               hasMenu: false,
@@ -43,7 +101,7 @@ const Home = () => {
             candidates[index].ids.push(id);
           }
         }
-      }
+      });
     });
     if (candidates.length > 0) {
       candidates.sort((a, b) => {
@@ -51,13 +109,10 @@ const Home = () => {
         if (a.count > b.count) return -1;
         return 0;
       });
-      const databases = candidates.map((v) => ({
-        name: v.name,
-        count: v.count,
-        hasMenu: false,
-        ids: v.ids.map((id) => ({ to: id })),
-      }));
-      console.log(databases);
+      const databases = candidates.map((v) => {
+        v.ids = v.ids.map((id) => ({ to: id }));
+        return v;
+      });
       setDatabaseNodes([databases]);
       setRoute([databases[0]]);
     }
