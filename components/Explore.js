2dfc74d9682118e15a7db13fcb86f5543074474b
Shuto Akazawa <akazawa@info-lounge.jp>
2021-05-17 15:52:24 +0900

    Add database info in explore

--- components/Explore.js
+++ components/Explore.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ResultModal from "../components/ResultModal";
 import { exportCSV, executeQuery } from "../lib/util";
+import dbConfig from "../public/config.json";
+import dbCatalogue from "../public/dataset.json";
 
 const Explore = (props) => {
   const [operationMenuVisibility, setOperationMenuVisibility] = useState(false);
@@ -8,6 +10,8 @@ const Explore = (props) => {
   const [tableData, setTableData] = useState({ heading: [], rows: [] });
   const [total, setTotal] = useState(0);
   const [menuVisibility, setMenuVisibility] = useState([null, null]);
+  const [databaseInfoVisibility, setdatabaseInfoVisibility] = useState(false);
+  const [database, setDatabase] = useState(null);
 
   useEffect(() => {
     if (tableData.heading.length > 0) setModalVisibility(true);
@@ -49,6 +53,14 @@ const Explore = (props) => {
 
     setTotal(d.total);
     setTableData({ heading, rows });
+  };
+
+  const showDatabaseInfo = async (v) => {
+    const dbName = Object.keys(dbCatalogue).filter((dataset) => dataset == v.name)
+    if (dbName.length == 1) {
+      setdatabaseInfoVisibility(true);
+      setDatabase(dbName);
+    }
   };
 
   return (
@@ -106,7 +118,7 @@ const Explore = (props) => {
                                 </svg>
                               </button>
 
-                              <button 
+                              <button
                                 onClick={handleExportCSV}
                                 className="action_icons__item">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 17">
@@ -114,9 +126,11 @@ const Explore = (props) => {
                                 </svg>
                               </button>
 
-                              <button className="action_icons__item">
+                              <button
+                                onClick={() => showDatabaseInfo(v)}
+                                className="action_icons__item">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6.427 16.004">
-                                  <path d="M13.5,4A1.5,1.5,0,1,0,15,5.5,1.5,1.5,0,0,0,13.5,4m-.36,4.77c-1.19.1-4.44,2.69-4.44,2.69-.2.15-.14.14.02.42s.14.29.33.16.53-.34,1.08-.68c2.12-1.36.34,1.78-.57,7.07-.36,2.62,2,1.27,2.61.87s2.21-1.5,2.37-1.61c.22-.15.06-.27-.11-.52-.12-.17-.24-.05-.24-.05-.65.43-1.84,1.33-2,.76-.19-.57,1.03-4.48,1.7-7.17C14,10.07,14.3,8.67,13.14,8.77Z" transform="translate(-8.573 -4)" fill="#fafafa"/>
+                                  <path d="M13.5,4A1.5,1.5,0,1,0,15,5.5,1.5,1.5,0,0,0,13.5,4m-.36,4.77c-1.19.1-4.44,2.69-4.44,2.69-.2.15-.14.14.02.42s.14.29.33.16.53-.34,1.08-.68c2.12-1.36.34,1.78-.57,7.07-.36,2.62,2,1.27,2.61.87s2.21-1.5,2.37-1.61c.22-.15.06-.27-.11-.52-.12-.17-.24-.05-.24-.05-.65.43-1.84,1.33-2,.76-.19-.57,1.03-4.48,1.7-7.17C14,10.07,14.3,8.67,13.14,8.77Z" transform="translate(-8.573 -4)" fill="#fafafa" />
                                 </svg>
                               </button>
                             </div>
@@ -134,6 +148,91 @@ const Explore = (props) => {
                     total={total}
                     setModalVisibility={setModalVisibility}
                   />
+                )}
+                {databaseInfoVisibility && (
+                  <div className="modal">
+                    <div className="modal__inner">
+                      <div className="modal__scroll_area">
+                        <button
+                          onClick={() => setdatabaseInfoVisibility(false)}
+                          className="modal__close"
+                        >
+                          <svg viewBox="0 0 24 24">
+                            <path
+                              fill="currentColor"
+                              d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+                            />
+                          </svg>
+                        </button>
+                        <article className="database__item">
+                          <h3 className="title">
+                            <img
+                              src="/images/icon_rat.png"
+                              alt="アイコン画像：ラット"
+                              className="icon"
+                            />
+                            <span className="text">{dbCatalogue[database].label}</span>
+                          </h3>
+                          <div className="description">
+                            {dbCatalogue[database].description}
+                          </div>
+
+                          {(() => {
+                            const labels = Object.keys(dbConfig).map(label => {
+                              if (label.indexOf(database) == 0) {
+                                const str = label.replace(`${database}-`, "")
+                                return (
+                                  <div htmlFor="result" className="path_label small green">
+                                    <span className="path_label__inner">
+                                      <img
+                                        src="/images/icon_rat.png"
+                                        alt="アイコン画像：ラット"
+                                        className="icon"
+                                      />
+                                      {str}
+                                    </span>
+                                  </div>
+                                )
+                              }
+                            }).filter(v => v)
+
+                            if (labels.length) {
+                              return (
+                                <div className="path">
+                                  <div className="path_label small white">LINK TO</div>
+                                  <svg className="icon" viewBox="0 0 24 24">
+                                    <path
+                                      fill="currentColor"
+                                      d="M4,15V9H12V4.16L19.84,12L12,19.84V15H4Z"
+                                    />
+                                  </svg>
+                                  {labels}
+                                </div>
+                              )
+                            }
+                          })()}
+                          
+
+                          <dl className="data">
+                            <div className="data__wrapper">
+                              <dt>PREFIX</dt>
+                              <dd>{dbCatalogue[database].prefix}</dd>
+                            </div>
+                            <div className="data__wrapper">
+                              <dt>CATEGORY</dt>
+                              <dd>{dbCatalogue[database].category}</dd>
+                            </div>
+                            {dbCatalogue[database].organization &&
+                              <div className="data__wrapper">
+                                <dt>ORGANIZATION</dt>
+                                <dd>{dbCatalogue[database].organization}</dd>
+                              </div>
+                            }
+                          </dl>
+                        </article>
+                      </div>
+                    </div>
+                  </div>
                 )}
               </div>
             </div>
