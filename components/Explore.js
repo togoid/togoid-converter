d087f3160eb09ba93dd352b984ef6cc0f90066ac
tanaka <tanaka@info-lounge.jp>
2021-05-17 14:16:43 +0900

    Add information modal

--- components/Explore.js
+++ components/Explore.js
@@ -8,6 +8,7 @@ const Explore = (props) => {
   const [tableData, setTableData] = useState({ heading: [], rows: [] });
   const [total, setTotal] = useState(0);
   const [menuVisibility, setMenuVisibility] = useState([null, null]);
+  const [informationModal, setInformationModal] = useState(false);
 
   useEffect(() => {
     if (tableData.heading.length > 0) setModalVisibility(true);
@@ -50,6 +51,13 @@ const Explore = (props) => {
     setTotal(d.total);
     setTableData({ heading, rows });
   };
+
+  const showInformationModal = async () => {
+    setInformationModal(true);
+  }
+  const hideInformationModal = async () => {
+    setInformationModal(false);
+  }
 
   return (
     <div className="explore">
@@ -96,6 +104,7 @@ const Explore = (props) => {
                                 </span>
                               </label>
                             </div>
+                            {console.log(v)}
                             <p className="result_count">999</p>
                             <div className="action_icons">
                               <button
@@ -112,7 +121,9 @@ const Explore = (props) => {
                                 </svg>
                               </button>
 
-                              <button className="action_icons__item">
+                              <button
+                                onClick={() => showInformationModal()}
+                                className="action_icons__item">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6.427 16.004">
                                   <path d="M13.5,4A1.5,1.5,0,1,0,15,5.5,1.5,1.5,0,0,0,13.5,4m-.36,4.77c-1.19.1-4.44,2.69-4.44,2.69-.2.15-.14.14.02.42s.14.29.33.16.53-.34,1.08-.68c2.12-1.36.34,1.78-.57,7.07-.36,2.62,2,1.27,2.61.87s2.21-1.5,2.37-1.61c.22-.15.06-.27-.11-.52-.12-.17-.24-.05-.24-.05-.65.43-1.84,1.33-2,.76-.19-.57,1.03-4.48,1.7-7.17C14,10.07,14.3,8.67,13.14,8.77Z" transform="translate(-8.573 -4)" fill="#fafafa"/>
                                 </svg>
@@ -123,6 +134,71 @@ const Explore = (props) => {
                       </ul>
                     </div>
                   ))}
+
+                {informationModal &&
+                <div className="modal modal--through">
+                  <div className="modal__inner modal__inner--through">
+                    <button
+                      onClick={() => hideInformationModal()}
+                      className="modal--through__close"/>
+                    <h2 className="modal--through__title">ChEMBL compound</h2>
+                    <p className="modal--through__description">
+                      ChEMBL is a manually curated database of bioactive molecules with drug-like properties. It brings together chemical, bioactivity and genomic data to aid the translation of genomic information into effective new drugs.
+                    </p>
+                    <div className="modal--through__buttons path">
+                      <div className="path_label small white">LINK TO</div>
+                      <svg className="arrow" viewBox="0 0 24 24">
+                        <path
+                          fill="currentColor"
+                          d="M4,15V9H12V4.16L19.84,12L12,19.84V15H4Z"
+                        />
+                      </svg>
+                      <div className="path_children">
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                        <div className="path_label small green">xxx</div>
+                      </div>
+                    </div>
+                    <dl className="modal--through__data_list">
+                      <div className="modal--through__data_list__item">
+                        <dt>PREFIX</dt>
+                        <dd>http://identifiers.org/chembl.compound/</dd>
+                      </div>
+
+                      <div className="modal--through__data_list__item">
+                        <dt>TYPE</dt>
+                        <dd>Chemical compound</dd>
+                      </div>
+
+                      <div className="modal--through__data_list__item">
+                        <dt>DATE</dt>
+                        <dd>2021/02/17</dd>
+                      </div>
+
+                      <div className="modal--through__data_list__item">
+                        <dt>HOST</dt>
+                        <dd>hogehoge</dd>
+                      </div>
+
+                      <div className="modal--through__data_list__item">
+                        <dt>RECORD</dt>
+                        <dd>765,460 entries</dd>
+                      </div>
+                    </dl>
+                  </div>
+                </div>
+                }
 
                 {modalVisibility && (
                   <ResultModal
