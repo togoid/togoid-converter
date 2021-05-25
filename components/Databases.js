6130f9326f2e750d2fe1f89c5e3b80f9df044434
Souta Ito <soutaito@info-lounge.jp>
2021-05-25 10:58:33 +0900

    Remove database search

--- components/Databases.js
+++ components/Databases.js
@@ -32,17 +32,6 @@ const Databases = () => {
               <label>ja</label>
             </div>
             <div className="database__inner">
-              <div className="database__top">
-                <div className="input_search">
-                  <svg className="input_search__icon" viewBox="0 0 24 24">
-                    <path
-                      fill="currentColor"
-                      d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
-                    />
-                  </svg>
-                  <input type="text" className="input_search__input" />
-                </div>
-              </div>
               {Object.keys(dataset).map((key) => {
                 const labels = Object.keys(dbConfig)
                   .map((k, i) => {
