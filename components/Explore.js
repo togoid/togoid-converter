3ddb0084bd3eed298426d0fd29d3d77c0faed5e9
aka0815 <52652603+aka0815@users.noreply.github.com>
2021-10-04 17:32:13 +0900

    Show candidatePaths

--- components/Explore.js
+++ components/Explore.js
@@ -129,132 +129,151 @@ const Explore = (props) => {
                             i === 0 ? "result_list first" : "result_list"
                           }
                         >
-                          {nodes.map((v, j) => (
-                            <li
-                              key={j}
-                              onMouseOver={() =>
-                                handleActionButtonVisibility(i, j)
-                              }
-                              onMouseLeave={() =>
-                                handleActionButtonVisibility(null, null)
-                              }
-                              className="result_list__item"
-                            >
-                              <div
-                                id={
-                                  j > 0 && nodes[j - 1].name === v.name
-                                    ? `node${i}-${v.name}-${j}`
-                                    : `node${i}-${v.name}`
-                                }
-                                className={`radio green ${
-                                  i === 0 || v.total > 0 ? null : "not_found"
-                                }`}
-                              >
-                                <input
-                                  type="radio"
-                                  name={`result${i}`}
-                                  id={`result${i}-${j}`}
-                                  className="radio__input"
-                                  checked={Boolean(
-                                    props.route[i] &&
-                                      props.route[i].name === v.name
-                                  )}
-                                  onChange={() => selectDatabase(v, i)}
-                                  disabled={i > 0 && !v.total}
-                                />
-                                <label
-                                  htmlFor={`result${i}-${j}`}
-                                  className="radio__large_label green"
-                                  style={{
-                                    opacity:
-                                      visibleActionButtonIndex[0] === i &&
-                                      visibleActionButtonIndex[1] === j
-                                        ? 0.7
-                                        : 1,
-                                    backgroundColor:
-                                      visibleActionButtonIndex[0] === i &&
-                                      visibleActionButtonIndex[1] === j
-                                        ? "#000000"
-                                        : categories[v.category]
-                                        ? categories[v.category].color
-                                        : null,
-                                  }}
+                          {nodes.map((v, j) => {
+                            if (!(j > 0 && nodes[j - 1].name === v.name)) {
+                              return (
+                                <li
+                                  key={j}
+                                  onMouseOver={() =>
+                                    handleActionButtonVisibility(i, j)
+                                  }
+                                  onMouseLeave={() =>
+                                    handleActionButtonVisibility(null, null)
+                                  }
+                                  className="result_list__item"
                                 >
-                                  <span
-                                    className="radio__large_label__inner"
-                                    style={{
-                                      color:
-                                        visibleActionButtonIndex[0] === i &&
-                                        visibleActionButtonIndex[1] === j
-                                          ? "#333333"
-                                          : "#ffffff",
-                                    }}
+                                  <div
+                                    id={`node${i}-${v.name}`}
+                                    className={`radio green ${
+                                      i === 0 || v.total > 0
+                                        ? null
+                                        : "not_found"
+                                    }`}
                                   >
-                                    {props.dbCatalogue[v.name].label}
-                                  </span>
-                                </label>
-                                {visibleActionButtonIndex[0] === i &&
-                                  visibleActionButtonIndex[1] === j && (
-                                    <div className="action_icons">
-                                      {i > 0 && v.total > 0 && (
-                                        <button
-                                          onClick={() => showModal(v, i)}
-                                          className="action_icons__item"
-                                        >
-                                          <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 18 16"
-                                          >
-                                            <path
-                                              d="M5,4H19a2,2,0,0,1,2,2V18a2,2,0,0,1-2,2H5a2,2,0,0,1-2-2V6A2,2,0,0,1,5,4M5,8v4h6V8H5m8,0v4h6V8H13M5,14v4h6V14H5m8,0v4h6V14Z"
-                                              transform="translate(-3 -4)"
-                                              fill="#fff"
-                                            />
-                                          </svg>
-                                        </button>
+                                    <input
+                                      type="radio"
+                                      name={`result${i}`}
+                                      id={`result${i}-${j}`}
+                                      className="radio__input"
+                                      checked={Boolean(
+                                        props.route[i] &&
+                                          props.route[i].name === v.name
                                       )}
-
-                                      {i > 0 && v.total > 0 && (
-                                        <button
-                                          onClick={() => handleIdDownload(v, i)}
-                                          className="action_icons__item"
-                                        >
-                                          <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 14 17"
-                                          >
-                                            <path
-                                              d="M5,20H19V18H5M19,9H15V3H9V9H5l7,7Z"
-                                              transform="translate(-5 -3)"
-                                              fill="#fff"
-                                            />
-                                          </svg>
-                                        </button>
-                                      )}
-
-                                      <button
-                                        onClick={() => showInformationModal(v)}
-                                        className="action_icons__item"
+                                      onChange={() => selectDatabase(v, i)}
+                                      disabled={i > 0 && !v.total}
+                                    />
+                                    <label
+                                      htmlFor={`result${i}-${j}`}
+                                      className="radio__large_label green"
+                                      style={{
+                                        opacity:
+                                          visibleActionButtonIndex[0] === i &&
+                                          visibleActionButtonIndex[1] === j
+                                            ? 0.7
+                                            : 1,
+                                        backgroundColor:
+                                          visibleActionButtonIndex[0] === i &&
+                                          visibleActionButtonIndex[1] === j
+                                            ? "#000000"
+                                            : categories[v.category]
+                                            ? categories[v.category].color
+                                            : null,
+                                      }}
+                                    >
+                                      <span
+                                        className="radio__large_label__inner"
+                                        style={{
+                                          color:
+                                            visibleActionButtonIndex[0] === i &&
+                                            visibleActionButtonIndex[1] === j
+                                              ? "#333333"
+                                              : "#ffffff",
+                                        }}
                                       >
-                                        <svg
-                                          xmlns="http://www.w3.org/2000/svg"
-                                          viewBox="0 0 6.427 16.004"
-                                        >
-                                          <path
-                                            d="M13.5,4A1.5,1.5,0,1,0,15,5.5,1.5,1.5,0,0,0,13.5,4m-.36,4.77c-1.19.1-4.44,2.69-4.44,2.69-.2.15-.14.14.02.42s.14.29.33.16.53-.34,1.08-.68c2.12-1.36.34,1.78-.57,7.07-.36,2.62,2,1.27,2.61.87s2.21-1.5,2.37-1.61c.22-.15.06-.27-.11-.52-.12-.17-.24-.05-.24-.05-.65.43-1.84,1.33-2,.76-.19-.57,1.03-4.48,1.7-7.17C14,10.07,14.3,8.67,13.14,8.77Z"
-                                            transform="translate(-8.573 -4)"
-                                            fill="#fafafa"
-                                          />
-                                        </svg>
-                                      </button>
-                                    </div>
+                                        {props.dbCatalogue[v.name].label}
+                                      </span>
+                                    </label>
+                                    {visibleActionButtonIndex[0] === i &&
+                                      visibleActionButtonIndex[1] === j && (
+                                        <div className="action_icons">
+                                          {i > 0 && v.total > 0 && (
+                                            <button
+                                              onClick={() => showModal(v, i)}
+                                              className="action_icons__item"
+                                            >
+                                              <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 18 16"
+                                              >
+                                                <path
+                                                  d="M5,4H19a2,2,0,0,1,2,2V18a2,2,0,0,1-2,2H5a2,2,0,0,1-2-2V6A2,2,0,0,1,5,4M5,8v4h6V8H5m8,0v4h6V8H13M5,14v4h6V14H5m8,0v4h6V14Z"
+                                                  transform="translate(-3 -4)"
+                                                  fill="#fff"
+                                                />
+                                              </svg>
+                                            </button>
+                                          )}
+
+                                          {i > 0 && v.total > 0 && (
+                                            <button
+                                              onClick={() =>
+                                                handleIdDownload(v, i)
+                                              }
+                                              className="action_icons__item"
+                                            >
+                                              <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 14 17"
+                                              >
+                                                <path
+                                                  d="M5,20H19V18H5M19,9H15V3H9V9H5l7,7Z"
+                                                  transform="translate(-5 -3)"
+                                                  fill="#fff"
+                                                />
+                                              </svg>
+                                            </button>
+                                          )}
+
+                                          <button
+                                            onClick={() =>
+                                              showInformationModal(v)
+                                            }
+                                            className="action_icons__item"
+                                          >
+                                            <svg
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              viewBox="0 0 6.427 16.004"
+                                            >
+                                              <path
+                                                d="M13.5,4A1.5,1.5,0,1,0,15,5.5,1.5,1.5,0,0,0,13.5,4m-.36,4.77c-1.19.1-4.44,2.69-4.44,2.69-.2.15-.14.14.02.42s.14.29.33.16.53-.34,1.08-.68c2.12-1.36.34,1.78-.57,7.07-.36,2.62,2,1.27,2.61.87s2.21-1.5,2.37-1.61c.22-.15.06-.27-.11-.52-.12-.17-.24-.05-.24-.05-.65.43-1.84,1.33-2,.76-.19-.57,1.03-4.48,1.7-7.17C14,10.07,14.3,8.67,13.14,8.77Z"
+                                                transform="translate(-8.573 -4)"
+                                                fill="#fafafa"
+                                              />
+                                            </svg>
+                                          </button>
+                                        </div>
+                                      )}
+                                  </div>
+                                  {(!props.isSpecific ||
+                                    i !==
+                                      props.databaseNodesList.length - 1) && (
+                                    <p
+                                      id={`total${i}-${v.name}`}
+                                      className="total"
+                                    >
+                                      {props.isSpecific &&
+                                      i === props.databaseNodesList.length - 2
+                                        ? props.databaseNodesList[i + 1][j]
+                                            .total
+                                        : v.total >= 0
+                                        ? v.total
+                                        : "too many"}
+                                    </p>
                                   )}
-                              </div>
-                              <p id={`total${i}-${v.name}`} className="total">
-                                {v.total >= 0 ? v.total : "too many"}
-                              </p>
-                            </li>
-                          ))}
+                                </li>
+                              );
+                            }
+                          })}
                         </ul>
                         {!props.isSpecific &&
                           i !== 0 &&
