fe932cb9128e7e7a55979c43c2620d0054f6dc31
sakzw <52652603+sakzw@users.noreply.github.com>
2022-05-19 09:16:57 +0900

    Change reorder EXPLORE and NAVIGATE

--- components/TabWrapper.js
+++ components/TabWrapper.js
@@ -1,20 +1,20 @@
 const TabWrapper = (props) => (
   <div className="tab_wrapper">
     <button
-      onClick={() => props.changeIndexTab("NAVIGATE")}
-      className={
-        props.activeTab === "NAVIGATE" ? "button_tab active" : "button_tab"
-      }
-    >
-      NAVIGATE
-    </button>
-    <button
       onClick={() => props.changeIndexTab("EXPLORE")}
       className={
         props.activeTab === "EXPLORE" ? "button_tab active" : "button_tab"
       }
     >
       EXPLORE
+    </button>
+    <button
+      onClick={() => props.changeIndexTab("NAVIGATE")}
+      className={
+        props.activeTab === "NAVIGATE" ? "button_tab active" : "button_tab"
+      }
+    >
+      NAVIGATE
     </button>
     <button
       onClick={() => props.changeIndexTab("DATABASE")}
