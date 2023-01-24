3f0c9a46ab1ca2e95192d965cca255c80c5a3b87
akazawa <52652603+sakzw@users.noreply.github.com>
2023-01-24 18:57:53 +0900

    ADD format mode button

--- components/ResultModalAction.js
+++ components/ResultModalAction.js
@@ -14,6 +14,7 @@ const previewModeList = new Map([
 
 const ResultModalAction = (props) => {
   const [previewMode, setPreviewMode] = useState("all");
+  const [formatMode, setFormatMode] = useState("Expanded");
   const [lineMode, setLineMode] = useState(
     Array(props.tableData.heading.length).fill("id")
   );
@@ -241,168 +242,200 @@ const ResultModalAction = (props) => {
       <div className="modal__top">
         <div className="item_wrapper">
           {props.tableData && props.tableData.rows.length > 0 && (
-            <div className="report">
-              <p className="modal__heading">Report</p>
-              <div className="report__inner">
-                {[...previewModeList]
-                  .filter(([key]) => key !== "full")
-                  .map(([key, value], i) => (
-                    <div className="radio" key={i}>
-                      <input
-                        id={i}
-                        key={i}
-                        value={key}
-                        name="report"
-                        type="radio"
-                        className="radio__input"
-                        checked={previewMode === key}
-                        onChange={() => setPreviewMode(key)}
-                      />
-                      <label htmlFor={i} className="radio__label">
-                        {value}
-                      </label>
-                    </div>
-                  ))}
-              </div>
-              <div className="report__inner">
-                <div className="radio" key={previewModeList.size - 1}>
-                  <input
-                    id={previewModeList.size - 1}
-                    key={previewModeList.size - 1}
-                    value="full"
-                    name="report"
-                    type="radio"
-                    className="radio__input"
-                    checked={previewMode === "full"}
-                    onChange={() => setPreviewMode("full")}
-                  />
-                  <label
-                    htmlFor={previewModeList.size - 1}
-                    className="radio__label"
-                  >
-                    {previewModeList.get("full")}
-                  </label>
+            <>
+              <div className="report">
+                <p className="modal__heading">Report</p>
+                <div className="report__inner">
+                  {[...previewModeList]
+                    .filter(([key]) => key !== "full")
+                    .map(([key, value], i) => (
+                      <div className="radio" key={i}>
+                        <input
+                          id={i}
+                          key={i}
+                          value={key}
+                          name="report"
+                          type="radio"
+                          className="radio__input"
+                          checked={previewMode === key}
+                          onChange={() => setPreviewMode(key)}
+                        />
+                        <label htmlFor={i} className="radio__label">
+                          {value}
+                        </label>
+                      </div>
+                    ))}
+                </div>
+                <div className="report__inner">
+                  <div className="radio" key={previewModeList.size - 1}>
+                    <input
+                      id={previewModeList.size - 1}
+                      key={previewModeList.size - 1}
+                      value="full"
+                      name="report"
+                      type="radio"
+                      className="radio__input"
+                      checked={previewMode === "full"}
+                      onChange={() => setPreviewMode("full")}
+                    />
+                    <label
+                      htmlFor={previewModeList.size - 1}
+                      className="radio__label"
+                    >
+                      {previewModeList.get("full")}
+                    </label>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
 
-          {props.tableData && props.tableData.rows.length > 0 && (
-            <div className="action">
-              <p className="modal__heading">Action</p>
-              <div className="action__inner">
-                <button
-                  onClick={() => handleExportCsvTsv("csv")}
-                  className="button_icon"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="11.497"
-                    height="13.961"
-                    viewBox="0 0 11.497 13.961"
-                    className="button_icon__icon"
-                  >
-                    <path
-                      id="download"
-                      d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
-                      transform="translate(-5 -3)"
-                      fill="#fff"
+              <div className="report">
+                <p className="modal__heading">Format</p>
+                <div className="report__inner">
+                  <div className="radio">
+                    <input
+                      id="expanded"
+                      name="format"
+                      type="radio"
+                      className="radio__input"
+                      checked={formatMode === "Expanded"}
+                      onChange={() => setFormatMode("Expanded")}
                     />
-                  </svg>
-                  Download as CSV
-                </button>
-                <button
-                  onClick={() => handleExportCsvTsv("tsv")}
-                  className="button_icon"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="11.497"
-                    height="13.961"
-                    viewBox="0 0 11.497 13.961"
-                    className="button_icon__icon"
-                  >
-                    <path
-                      id="download"
-                      d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
-                      transform="translate(-5 -3)"
-                      fill="#fff"
-                    />
-                  </svg>
-                  Download as TSV
-                </button>
-                <ResultModalClipboardButton copyFunction={copyClipboard}>
-                  Copy to clipboard
-                </ResultModalClipboardButton>
-                <ResultModalClipboardButton copyFunction={copyClipboardURL}>
-                  Copy API URL
-                </ResultModalClipboardButton>
-              </div>
-              <div className="action__inner">
-                <button
-                  onClick={() => handleExportCsvTsv("csv", true)}
-                  className="button_icon"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="11.497"
-                    height="13.961"
-                    viewBox="0 0 11.497 13.961"
-                    className="button_icon__icon"
-                  >
-                    <path
-                      id="download"
-                      d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
-                      transform="translate(-5 -3)"
-                      fill="#fff"
-                    />
-                  </svg>
-                  Download as reduced CSV
-                </button>
-                <button
-                  onClick={() => handleExportCsvTsv("tsv", true)}
-                  className="button_icon"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="11.497"
-                    height="13.961"
-                    viewBox="0 0 11.497 13.961"
-                    className="button_icon__icon"
-                  >
-                    <path
-                      id="download"
-                      d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
-                      transform="translate(-5 -3)"
-                      fill="#fff"
-                    />
-                  </svg>
-                  Download as reduced TSV
-                </button>
-                <ResultModalClipboardButton
-                  copyFunction={copyClipboard}
-                  isReduced={true}
-                >
-                  Copy reduced to clipboard
-                </ResultModalClipboardButton>
-                <ResultModalClipboardButton
-                  copyFunction={copyClipboardURL}
-                  isReduced={true}
-                >
-                  Copy reduced API URL
-                </ResultModalClipboardButton>
-              </div>
-              {props.lastTargetCount === "10000+" && (
-                <div>
-                  <div>
-                    Warning : There is a lot of data and errors may occur
+                    <label htmlFor="expanded" className="radio__label">
+                      Expanded
+                    </label>
                   </div>
-                  <div>
-                    Please consider using the API limit and offset functions
+                  <div className="radio">
+                    <input
+                      id="compact"
+                      name="format"
+                      type="radio"
+                      className="radio__input"
+                      checked={formatMode === "Compact"}
+                      onChange={() => setFormatMode("Compact")}
+                    />
+                    <label htmlFor="compact" className="radio__label">
+                      Compact
+                    </label>
                   </div>
                 </div>
-              )}
-            </div>
+              </div>
+
+              <div className="action">
+                <p className="modal__heading">Action</p>
+                <div className="action__inner">
+                  <button
+                    onClick={() => handleExportCsvTsv("csv")}
+                    className="button_icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="11.497"
+                      height="13.961"
+                      viewBox="0 0 11.497 13.961"
+                      className="button_icon__icon"
+                    >
+                      <path
+                        id="download"
+                        d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
+                        transform="translate(-5 -3)"
+                        fill="#fff"
+                      />
+                    </svg>
+                    Download as CSV
+                  </button>
+                  <button
+                    onClick={() => handleExportCsvTsv("tsv")}
+                    className="button_icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="11.497"
+                      height="13.961"
+                      viewBox="0 0 11.497 13.961"
+                      className="button_icon__icon"
+                    >
+                      <path
+                        id="download"
+                        d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
+                        transform="translate(-5 -3)"
+                        fill="#fff"
+                      />
+                    </svg>
+                    Download as TSV
+                  </button>
+                  <ResultModalClipboardButton copyFunction={copyClipboard}>
+                    Copy to clipboard
+                  </ResultModalClipboardButton>
+                  <ResultModalClipboardButton copyFunction={copyClipboardURL}>
+                    Copy API URL
+                  </ResultModalClipboardButton>
+                </div>
+                <div className="action__inner">
+                  <button
+                    onClick={() => handleExportCsvTsv("csv", true)}
+                    className="button_icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="11.497"
+                      height="13.961"
+                      viewBox="0 0 11.497 13.961"
+                      className="button_icon__icon"
+                    >
+                      <path
+                        id="download"
+                        d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
+                        transform="translate(-5 -3)"
+                        fill="#fff"
+                      />
+                    </svg>
+                    Download as reduced CSV
+                  </button>
+                  <button
+                    onClick={() => handleExportCsvTsv("tsv", true)}
+                    className="button_icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="11.497"
+                      height="13.961"
+                      viewBox="0 0 11.497 13.961"
+                      className="button_icon__icon"
+                    >
+                      <path
+                        id="download"
+                        d="M5,16.961H16.5V15.319H5M16.5,7.927H13.212V3H8.285V7.927H5l5.749,5.749Z"
+                        transform="translate(-5 -3)"
+                        fill="#fff"
+                      />
+                    </svg>
+                    Download as reduced TSV
+                  </button>
+                  <ResultModalClipboardButton
+                    copyFunction={copyClipboard}
+                    isReduced={true}
+                  >
+                    Copy reduced to clipboard
+                  </ResultModalClipboardButton>
+                  <ResultModalClipboardButton
+                    copyFunction={copyClipboardURL}
+                    isReduced={true}
+                  >
+                    Copy reduced API URL
+                  </ResultModalClipboardButton>
+                </div>
+                {props.lastTargetCount === "10000+" && (
+                  <div>
+                    <div>
+                      Warning : There is a lot of data and errors may occur
+                    </div>
+                    <div>
+                      Please consider using the API limit and offset functions
+                    </div>
+                  </div>
+                )}
+              </div>
+            </>
           )}
         </div>
         {filterTable?.rows?.length > 0 && (
