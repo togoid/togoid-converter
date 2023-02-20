f4acca40f662cf000520bc31dfb2c2fda899c937
tanaka <tanaka@info-lounge.jp>
2023-02-20 16:56:28 +0900

    MarkUp (Annotate page)

--- components/Annotate.js
+++ components/Annotate.js
@@ -5,7 +5,51 @@ import React from "react";
  *
  */
 const Annotate = () => {
-  return <div>Annotate</div>;
+  return (
+    <div className="annotate">
+      <nav className="annotate__side-menu">
+        <button className="annotate__side-menu__button green active">
+          <span className="text">HGNC</span>
+          <span className="total">999</span>
+        </button>
+        <button className="annotate__side-menu__button green">
+          <span className="text">HGNC</span>
+          <span className="total">999</span>
+        </button>
+        <button className="annotate__side-menu__button orange">
+          <span className="text">HGNC</span>
+          <span className="total">999</span>
+        </button>
+      </nav>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>URI</th>
+            <th>ID</th>
+            <th>LABEL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Xxx-xxx-xxxx</td>
+            <td>YYY-YYY-YYY</td>
+            <td>Zzz-zzz-zzzz</td>
+          </tr>
+          <tr>
+            <td>Xxx-xxx-xxxx</td>
+            <td>YYY-YYY-YYY</td>
+            <td>Zzz-zzz-zzzz</td>
+          </tr>
+          <tr>
+            <td>Xxx-xxx-xxxx</td>
+            <td>YYY-YYY-YYY</td>
+            <td>Zzz-zzz-zzzz</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Annotate;
