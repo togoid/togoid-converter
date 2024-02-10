7a9c36507f60dbef42329ea0101734c76e1371d1
akazawa <akazawa@info-lounge.jp>
2024-02-10 17:49:45 +0900

    explore

---
+++ src/components/Explore.tsx
@@ -0,0 +1,430 @@
+import React from "react";
+// import ResultModal from "../components/ResultModal";
+// import InformationModal from "../components/InformationModal";
+import { ArrowArea } from "react-arrow-master";
+
+const sortConfig = {
+  name: { up: "desc", down: "asc" },
+  category: { up: "desc", down: "asc" },
+  sourceCount: { up: "asc", down: "desc" },
+  targetCount: { up: "asc", down: "desc" },
+};
+
+const Explore = (props) => {
+  const [modalVisibility, setModalVisibility] = useState(false);
+  const [tableData, setTableData] = useState({ heading: [] });
+  const [informationModal, setInformationModal] = useState(false);
+  const [database, setDatabase] = useState(null);
+  const [visibleActionButtonIndex, setVisibleActionButtonIndex] = useState([
+    null,
+    null,
+  ]);
+  const [convertedCount, setConvertedCount] = useState([]);
+  const [nodesList, setNodesList] = useState([]);
+  const [sortModeOrderList, setSortModeOrderList] = useState([]);
+
+  const { datasetConfig } = useConfig();
+
+  useEffect(() => {
+    if (tableData.heading.length > 0) setModalVisibility(true);
+  }, [tableData]);
+
+  useEffect(() => {
+    // 直前の状態と比較して変化していればnameとascでソートする
+    const sortNodes = [];
+    const sortModeOrders = [];
+    props.databaseNodesList.forEach((v, i) => {
+      if (v === nodesList[i]) {
+        sortNodes.push(v);
+        sortModeOrders.push(sortModeOrderList[i]);
+      } else {
+        const sorted = sortFunc(v, "name", "down");
+
+        sortNodes.push(sorted);
+        sortModeOrders.push({ mode: "name", direction: "down" });
+      }
+    });
+    setNodesList(sortNodes);
+    setSortModeOrderList(sortModeOrders);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.databaseNodesList]);
+
+  const sortNode = (mode, direction, i) => {
+    // この時点で元の配列が壊れても気にしないので shallow copy する
+    const nodesListCopy = nodesList.slice();
+    nodesListCopy[i] = sortFunc(nodesListCopy[i], mode, direction);
+
+    setNodesList(nodesListCopy);
+
+    const sortModeOrderListCopy = sortModeOrderList.slice();
+    sortModeOrderListCopy[i] = {
+      mode,
+      direction,
+    };
+    setSortModeOrderList(sortModeOrderListCopy);
+  };
+
+  const sortFunc = (item, mode, direction) => {
+    const n = sortConfig[mode][direction] === "asc" ? 1 : -1;
+
+    if (mode === "name") {
+      item.sort((a, b) => {
+        if (
+          datasetConfig[a.name].label.toLowerCase() <
+          datasetConfig[b.name].label.toLowerCase()
+        ) {
+          return n * -1;
+        } else if (
+          datasetConfig[a.name].label.toLowerCase() >
+          datasetConfig[b.name].label.toLowerCase()
+        ) {
+          return n * 1;
+        }
+      });
+    } else if (mode === "category") {
+      item.sort((a, b) => {
+        if (a.category < b.category) {
+          return n * -1;
+        } else if (a.category > b.category) {
+          return n * 1;
+        }
+      });
+    } else if (mode === "sourceCount") {
+      item.sort((a, b) => (a.source - b.source) * n);
+    } else if (mode === "targetCount") {
+      item.sort((a, b) => (a.target - b.target) * n);
+    }
+
+    return item;
+  };
+
+  const selectDatabase = (database, i) => {
+    const r = props.route.slice(0, i);
+    r[i] = database;
+    props.setRoute(r);
+    props.setPreviousRoute(r);
+    return r;
+  };
+
+  const handleIdDownload = async (database, routeIndex) => {
+    const r = selectDatabase(database, routeIndex).slice(0, routeIndex + 1);
+    const d = await executeQuery({
+      route: r,
+      ids: props.ids,
+      report: "target",
+    });
+
+    const prefix = datasetConfig[database.name].prefix.split("/").slice(-1);
+
+    exportCsvTsv(
+      d.results.map((result) => [prefix + result]),
+      "tsv",
+      "ids.tsv",
+    );
+  };
+
+  const showModal = async (database, routeIndex) => {
+    const r = selectDatabase(database, routeIndex).slice(0, routeIndex + 1);
+    const heading = r
+      .filter((v, i) => i <= routeIndex)
+      .map((v) => datasetConfig[v.name]);
+
+    setTableData({ heading });
+    const counts = r.map((v) => {
+      const source = v.message
+        ? v.message === "ERROR"
+          ? v.message
+          : "-"
+        : v.source;
+      const target = v.message ? v.message : v.target;
+      return { source: source, target: target };
+    });
+    setConvertedCount(counts);
+  };
+
+  const showInformationModal = (v) => {
+    setInformationModal(true);
+    setDatabase(v.name);
+  };
+
+  const handleActionButtonVisibility = (i, j) => {
+    setVisibleActionButtonIndex([i, j]);
+  };
+
+  return (
+    <div className="explore">
+      <div className="drawing_area">
+        <div className="panel">
+          <div className="panel__inner">
+            <div className="explore">
+              <ArrowArea arrows={props.candidatePaths}>
+                <div className="drawing">
+                  {nodesList.map((nodes, i) => (
+                    <React.Fragment key={i}>
+                      {i !== 0 && (
+                        <div className="item_wrapper" key={1}>
+                          <ul className="label_list with_sort" key={2}>
+                            {nodes.map((v, j) => {
+                              return (
+                                <li key={j} className="label_list__item">
+                                  <p
+                                    id={`label${i}-${v.name}`}
+                                    className="label_list__item__inner"
+                                  >
+                                    {v.link}
+                                  </p>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                      )}
+                      <div className="item_wrapper" key={2}>
+                        <div
+                          className={
+                            i === 0
+                              ? "item_wrapper__buttons first"
+                              : "item_wrapper__buttons"
+                          }
+                          key={1}
+                        >
+                          <label className="sort__label" htmlFor="sort">
+                            Sort by
+                          </label>
+                          <select
+                            id="sort"
+                            className="select white"
+                            onChange={(e) =>
+                              sortNode(
+                                e.target.value,
+                                sortModeOrderList[i].direction,
+                                i,
+                              )
+                            }
+                            value={sortModeOrderList[i].mode}
+                          >
+                            <option value="name">Name</option>
+                            <option value="category">Category</option>
+                            {i !== 0 && (
+                              <>
+                                <option value="sourceCount">
+                                  Source count
+                                </option>
+                                <option value="targetCount">
+                                  Target count
+                                </option>
+                              </>
+                            )}
+                          </select>
+                          <div className="sort">
+                            <button
+                              onClick={() =>
+                                sortNode(
+                                  sortModeOrderList[i].mode,
+                                  sortModeOrderList[i].direction === "up"
+                                    ? "down"
+                                    : "up",
+                                  i,
+                                )
+                              }
+                              className={`sort__button`}
+                            >
+                              <div
+                                className={`sort__button up ${
+                                  sortModeOrderList[i].direction === "up"
+                                    ? "active"
+                                    : ""
+                                }`}
+                              />
+                              <div
+                                className={`sort__button down ${
+                                  sortModeOrderList[i].direction === "down"
+                                    ? "active"
+                                    : ""
+                                }`}
+                              />
+                            </button>
+                          </div>
+                        </div>
+
+                        <ul
+                          className={
+                            i === 0 ? "result_list first" : "result_list"
+                          }
+                          key={2}
+                        >
+                          {nodes.map((v, j) => {
+                            const isActionButtonVisible =
+                              visibleActionButtonIndex[0] === i &&
+                              visibleActionButtonIndex[1] === j;
+                            return (
+                              // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
+                              <li
+                                key={j}
+                                onMouseOver={() =>
+                                  handleActionButtonVisibility(i, j)
+                                }
+                                onMouseLeave={() =>
+                                  handleActionButtonVisibility(null, null)
+                                }
+                                className="result_list__item"
+                              >
+                                <div
+                                  id={`to${i}-${v.name}`}
+                                  className={`radio green ${
+                                    i === 0 || v.target > 0 ? null : "not_found"
+                                  }`}
+                                >
+                                  <input
+                                    type="radio"
+                                    name={`result${i}`}
+                                    id={`result${i}-${j}`}
+                                    className="radio__input"
+                                    checked={Boolean(
+                                      props.route[i] &&
+                                        props.route[i].name === v.name,
+                                    )}
+                                    onChange={() => selectDatabase(v, i)}
+                                    disabled={i > 0 && !v.target}
+                                  />
+                                  <label
+                                    htmlFor={`result${i}-${j}`}
+                                    className="radio__large_label green"
+                                    style={{
+                                      backgroundColor: isActionButtonVisible
+                                        ? "#000000"
+                                        : categories[v.category]
+                                          ? categories[v.category].color
+                                          : null,
+                                    }}
+                                  >
+                                    <div
+                                      id={`from${i}-${v.name}`}
+                                      className="dummy"
+                                    />
+                                    <p
+                                      className="radio__large_label__inner"
+                                      style={{
+                                        color: isActionButtonVisible
+                                          ? "#333333"
+                                          : "#ffffff",
+                                      }}
+                                    >
+                                      {i > 0 ? (
+                                        <span
+                                          id={`converted${i}-${v.name}`}
+                                          className="total"
+                                        >
+                                          {v.message
+                                            ? v.message === "ERROR"
+                                              ? v.message
+                                              : "-"
+                                            : v.source}
+                                        </span>
+                                      ) : (
+                                        <span
+                                          id={`converted${i}-${v.name}`}
+                                        ></span>
+                                      )}
+                                      <span className="text">
+                                        {datasetConfig[v.name].label}
+                                      </span>
+                                      <span
+                                        id={`total${i}-${v.name}`}
+                                        className="total"
+                                      >
+                                        {v.message ? v.message : v.target}
+                                      </span>
+                                    </p>
+                                  </label>
+                                  {isActionButtonVisible && (
+                                    <div className="action_icons">
+                                      {i > 0 && v.target > 0 && (
+                                        <button
+                                          onClick={() => showModal(v, i)}
+                                          className="action_icons__item"
+                                        >
+                                          <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 18 16"
+                                          >
+                                            <path
+                                              d="M5,4H19a2,2,0,0,1,2,2V18a2,2,0,0,1-2,2H5a2,2,0,0,1-2-2V6A2,2,0,0,1,5,4M5,8v4h6V8H5m8,0v4h6V8H13M5,14v4h6V14H5m8,0v4h6V14Z"
+                                              transform="translate(-3 -4)"
+                                              fill="#fff"
+                                            />
+                                          </svg>
+                                        </button>
+                                      )}
+
+                                      {i > 0 && v.target > 0 && (
+                                        <button
+                                          onClick={() => handleIdDownload(v, i)}
+                                          className="action_icons__item"
+                                        >
+                                          <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 14 17"
+                                          >
+                                            <path
+                                              d="M5,20H19V18H5M19,9H15V3H9V9H5l7,7Z"
+                                              transform="translate(-5 -3)"
+                                              fill="#fff"
+                                            />
+                                          </svg>
+                                        </button>
+                                      )}
+
+                                      <button
+                                        onClick={() => showInformationModal(v)}
+                                        className="action_icons__item"
+                                      >
+                                        <svg
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          viewBox="0 0 6.427 16.004"
+                                        >
+                                          <path
+                                            d="M13.5,4A1.5,1.5,0,1,0,15,5.5,1.5,1.5,0,0,0,13.5,4m-.36,4.77c-1.19.1-4.44,2.69-4.44,2.69-.2.15-.14.14.02.42s.14.29.33.16.53-.34,1.08-.68c2.12-1.36.34,1.78-.57,7.07-.36,2.62,2,1.27,2.61.87s2.21-1.5,2.37-1.61c.22-.15.06-.27-.11-.52-.12-.17-.24-.05-.24-.05-.65.43-1.84,1.33-2,.76-.19-.57,1.03-4.48,1.7-7.17C14,10.07,14.3,8.67,13.14,8.77Z"
+                                            transform="translate(-8.573 -4)"
+                                            fill="#fafafa"
+                                          />
+                                        </svg>
+                                      </button>
+                                    </div>
+                                  )}
+                                </div>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                    </React.Fragment>
+                  ))}
+
+                  {/* {informationModal && (
+                    <InformationModal
+                      setInformationModal={setInformationModal}
+                      database={database}
+                    />
+                  )}
+
+                  {modalVisibility && (
+                    <ResultModal
+                      route={props.route}
+                      ids={props.ids}
+                      tableData={tableData}
+                      setModalVisibility={setModalVisibility}
+                      convertedCount={convertedCount}
+                    />
+                  )} */}
+                </div>
+              </ArrowArea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
---
+++ src/components/IdInput.tsx
@@ -0,0 +1,147 @@
+const IdInput = (props) => {
+  const [text, setText] = useState(props.idTexts);
+
+  useEffect(() => {
+    setText(props.idTexts);
+  }, [props.idTexts]);
+
+  const handleSubmit = (e) => {
+    if (e) e.preventDefault();
+
+    const findDatabaseList = props.handleIdTextsSubmit(text);
+    if (props.previousRoute.length) {
+      const firstRoute = findDatabaseList.find(
+        (v) => v.name === props.previousRoute[0].name,
+      );
+      if (firstRoute) {
+        // keepRouteを使用する
+        props.setRoute([firstRoute]);
+        props.setIsUseKeepRoute(true);
+        return;
+      }
+    }
+
+    if (findDatabaseList.length === 1) {
+      // listが1件の時は自動で選択する
+      props.setRoute(findDatabaseList);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if ((e.ctrlKey || e.shiftKey) && e.keyCode === 13) {
+      handleSubmit();
+    }
+  };
+
+  const selectTextFile = () => {
+    // @ts-expect-error
+    inputRef.current.click();
+  };
+  const inputRef = useRef();
+
+  const readTextFile = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = () => {
+      props.setIdTexts(reader.result);
+      props.handleIdTextsSubmit(reader.result);
+    };
+    reader.onerror = () => {
+      console.log(reader.error);
+    };
+
+    e.target.value = "";
+  };
+
+  const handleReset = () => {
+    props.restartExplore();
+  };
+
+  return (
+    <div className="input_area">
+      <form onSubmit={handleSubmit} className="textarea">
+        <div className="textarea_wrapper">
+          <textarea
+            cols={30}
+            rows={10}
+            placeholder="Input your ID (set), separated by comma, space, or newline (e.g. 5460, 6657, 9314, 4609 for NCBI gene)."
+            className="textarea__input"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          {props.idTexts && (
+            <button
+              onClick={() => props.setIdTexts("")}
+              className="textarea_clear"
+            />
+          )}
+        </div>
+        <div className="input">
+          <input type="submit" value="Submit" className="button_large" />
+          <input
+            className="button_small"
+            type="button"
+            value="Input from text file"
+            id="csv"
+            name="input_type"
+            onClick={selectTextFile}
+          />
+          <input
+            type="file"
+            // @ts-expect-error
+            ref={inputRef}
+            style={{ display: "none" }}
+            onChange={readTextFile}
+          />
+          <input
+            className="button_small"
+            type="button"
+            value="Reset"
+            onClick={handleReset}
+          />
+        </div>
+      </form>
+
+      <div className="input_area__bottom">
+        <div className="input_area__bottom__links">
+          <p className="input_area__bottom__square">Examples:</p>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              props.handleTopExamples("refseq_rna");
+            }}
+            className="input_area__bottom__link"
+          >
+            Refseq RNA
+          </a>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              props.handleTopExamples("ensembl_gene");
+            }}
+            className="input_area__bottom__link"
+          >
+            Ensembl gene
+          </a>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              props.handleTopExamples("uniprot");
+            }}
+            className="input_area__bottom__link"
+          >
+            UniProt
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdInput;
