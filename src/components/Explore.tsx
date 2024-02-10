adfb21748822f3bf438fed69ceda133a87ce5bfd
akazawa <akazawa@info-lounge.jp>
2024-02-10 20:14:16 +0900

    split explore result

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -2,6 +2,7 @@ import React from "react";
 // import ResultModal from "../components/ResultModal";
 // import InformationModal from "../components/InformationModal";
 import { ArrowArea } from "react-arrow-master";
+import ExploreResultItem from "@/components/ExploreResultItem";
 
 const sortConfig = {
   name: { up: "desc", down: "asc" },
@@ -253,149 +254,16 @@ const Explore = (props) => {
                           }
                           key={2}
                         >
-                          {nodes.map((v, j) => {
-                            const isActionButtonVisible =
-                              visibleActionButtonIndex[0] === i &&
-                              visibleActionButtonIndex[1] === j;
-                            return (
-                              // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
-                              <li
-                                key={j}
-                                onMouseOver={() =>
-                                  handleActionButtonVisibility(i, j)
-                                }
-                                onMouseLeave={() =>
-                                  handleActionButtonVisibility(null, null)
-                                }
-                                className="result_list__item"
-                              >
-                                <div
-                                  id={`to${i}-${v.name}`}
-                                  className={`radio green ${
-                                    i === 0 || v.target > 0 ? null : "not_found"
-                                  }`}
-                                >
-                                  <input
-                                    type="radio"
-                                    name={`result${i}`}
-                                    id={`result${i}-${j}`}
-                                    className="radio__input"
-                                    checked={Boolean(
-                                      props.route[i] &&
-                                        props.route[i].name === v.name,
-                                    )}
-                                    onChange={() => selectDatabase(v, i)}
-                                    disabled={i > 0 && !v.target}
-                                  />
-                                  <label
-                                    htmlFor={`result${i}-${j}`}
-                                    className="radio__large_label green"
-                                    style={{
-                                      backgroundColor: isActionButtonVisible
-                                        ? "#000000"
-                                        : categories[v.category]
-                                          ? categories[v.category].color
-                                          : null,
-                                    }}
-                                  >
-                                    <div
-                                      id={`from${i}-${v.name}`}
-                                      className="dummy"
-                                    />
-                                    <p
-                                      className="radio__large_label__inner"
-                                      style={{
-                                        color: isActionButtonVisible
-                                          ? "#333333"
-                                          : "#ffffff",
-                                      }}
-                                    >
-                                      {i > 0 ? (
-                                        <span
-                                          id={`converted${i}-${v.name}`}
-                                          className="total"
-                                        >
-                                          {v.message
-                                            ? v.message === "ERROR"
-                                              ? v.message
-                                              : "-"
-                                            : v.source}
-                                        </span>
-                                      ) : (
-                                        <span
-                                          id={`converted${i}-${v.name}`}
-                                        ></span>
-                                      )}
-                                      <span className="text">
-                                        {datasetConfig[v.name].label}
-                                      </span>
-                                      <span
-                                        id={`total${i}-${v.name}`}
-                                        className="total"
-                                      >
-                                        {v.message ? v.message : v.target}
-                                      </span>
-                                    </p>
-                                  </label>
-                                  {isActionButtonVisible && (
-                                    <div className="action_icons">
-                                      {i > 0 && v.target > 0 && (
-                                        <button
-                                          onClick={() => showModal(v, i)}
-                                          className="action_icons__item"
-                                        >
-                                          <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 18 16"
-                                          >
-                                            <path
-                                              d="M5,4H19a2,2,0,0,1,2,2V18a2,2,0,0,1-2,2H5a2,2,0,0,1-2-2V6A2,2,0,0,1,5,4M5,8v4h6V8H5m8,0v4h6V8H13M5,14v4h6V14H5m8,0v4h6V14Z"
-                                              transform="translate(-3 -4)"
-                                              fill="#fff"
-                                            />
-                                          </svg>
-                                        </button>
-                                      )}
-
-                                      {i > 0 && v.target > 0 && (
-                                        <button
-                                          onClick={() => handleIdDownload(v, i)}
-                                          className="action_icons__item"
-                                        >
-                                          <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 14 17"
-                                          >
-                                            <path
-                                              d="M5,20H19V18H5M19,9H15V3H9V9H5l7,7Z"
-                                              transform="translate(-5 -3)"
-                                              fill="#fff"
-                                            />
-                                          </svg>
-                                        </button>
-                                      )}
-
-                                      <button
-                                        onClick={() => showInformationModal(v)}
-                                        className="action_icons__item"
-                                      >
-                                        <svg
-                                          xmlns="http://www.w3.org/2000/svg"
-                                          viewBox="0 0 6.427 16.004"
-                                        >
-                                          <path
-                                            d="M13.5,4A1.5,1.5,0,1,0,15,5.5,1.5,1.5,0,0,0,13.5,4m-.36,4.77c-1.19.1-4.44,2.69-4.44,2.69-.2.15-.14.14.02.42s.14.29.33.16.53-.34,1.08-.68c2.12-1.36.34,1.78-.57,7.07-.36,2.62,2,1.27,2.61.87s2.21-1.5,2.37-1.61c.22-.15.06-.27-.11-.52-.12-.17-.24-.05-.24-.05-.65.43-1.84,1.33-2,.76-.19-.57,1.03-4.48,1.7-7.17C14,10.07,14.3,8.67,13.14,8.77Z"
-                                            transform="translate(-8.573 -4)"
-                                            fill="#fafafa"
-                                          />
-                                        </svg>
-                                      </button>
-                                    </div>
-                                  )}
-                                </div>
-                              </li>
-                            );
-                          })}
+                          {nodes.map((v, j) => (
+                            <ExploreResultItem
+                              key={j}
+                              i={i}
+                              j={j}
+                              v={v}
+                              route={props.route}
+                              selectDatabase={selectDatabase}
+                            />
+                          ))}
                         </ul>
                       </div>
                     </React.Fragment>
