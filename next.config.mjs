d969cc22a50f79540cf11bafa0d62b05a4502b5c
akazawa <akazawa@info-lounge.jp>
2024-02-10 17:09:01 +0900

    auto import

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,24 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import AutoImport from "unplugin-auto-import/webpack";
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack: (config, options) => {
+    config.plugins.push(
+      AutoImport({
+        imports: ["react", "jotai", "jotai/utils"],
+        dirs: ["./src/hooks", "./src/lib", "./src/atoms"],
+      }),
+    );
+
+    return config;
   },
 };
 
