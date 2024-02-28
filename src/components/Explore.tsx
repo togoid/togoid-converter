352d1500a23be677b364650a5ead7d93b71282f0
akazawa <akazawa@info-lounge.jp>
2024-02-28 11:11:35 +0900

    explore result component

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,89 +1,8 @@
 import React from "react";
 import { ArrowArea } from "react-arrow-master";
-import ExploreResultItem from "@/components/ExploreResultItem";
-
-const sortConfig = {
-  name: { up: "desc", down: "asc" },
-  category: { up: "desc", down: "asc" },
-  sourceCount: { up: "asc", down: "desc" },
-  targetCount: { up: "asc", down: "desc" },
-};
+import ExploreResult from "@/components/ExploreResult";
 
 const Explore = (props) => {
-  const [nodesList, setNodesList] = useState([]);
-  const [sortModeOrderList, setSortModeOrderList] = useState([]);
-
-  const { datasetConfig } = useConfig();
-
-  useEffect(() => {
-    // 直前の状態と比較して変化していればnameとascでソートする
-    const sortNodes = [];
-    const sortModeOrders = [];
-    props.databaseNodesList.forEach((v, i) => {
-      if (v === nodesList[i]) {
-        sortNodes.push(v);
-        sortModeOrders.push(sortModeOrderList[i]);
-      } else {
-        const sorted = sortFunc(v, "name", "down");
-
-        sortNodes.push(sorted);
-        sortModeOrders.push({ mode: "name", direction: "down" });
-      }
-    });
-    setNodesList(sortNodes);
-    setSortModeOrderList(sortModeOrders);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.databaseNodesList]);
-
-  const sortNode = (mode, direction, i) => {
-    // この時点で元の配列が壊れても気にしないので shallow copy する
-    const nodesListCopy = nodesList.slice();
-    nodesListCopy[i] = sortFunc(nodesListCopy[i], mode, direction);
-
-    setNodesList(nodesListCopy);
-
-    const sortModeOrderListCopy = sortModeOrderList.slice();
-    sortModeOrderListCopy[i] = {
-      mode,
-      direction,
-    };
-    setSortModeOrderList(sortModeOrderListCopy);
-  };
-
-  const sortFunc = (item, mode, direction) => {
-    const n = sortConfig[mode][direction] === "asc" ? 1 : -1;
-
-    if (mode === "name") {
-      item.sort((a, b) => {
-        if (
-          datasetConfig[a.name].label.toLowerCase() <
-          datasetConfig[b.name].label.toLowerCase()
-        ) {
-          return n * -1;
-        } else if (
-          datasetConfig[a.name].label.toLowerCase() >
-          datasetConfig[b.name].label.toLowerCase()
-        ) {
-          return n * 1;
-        }
-      });
-    } else if (mode === "category") {
-      item.sort((a, b) => {
-        if (a.category < b.category) {
-          return n * -1;
-        } else if (a.category > b.category) {
-          return n * 1;
-        }
-      });
-    } else if (mode === "sourceCount") {
-      item.sort((a, b) => (a.source - b.source) * n);
-    } else if (mode === "targetCount") {
-      item.sort((a, b) => (a.target - b.target) * n);
-    }
-
-    return item;
-  };
-
   const selectDatabase = (database, i) => {
     const r = props.route.slice(0, i);
     r[i] = database;
@@ -100,96 +19,15 @@ const Explore = (props) => {
             <div className="explore">
               <ArrowArea arrows={props.candidatePaths}>
                 <div className="drawing">
-                  {nodesList.map((nodes, i) => (
-                    <div className="item_wrapper" key={i}>
-                      <div
-                        className={
-                          i === 0
-                            ? "item_wrapper__buttons first"
-                            : "item_wrapper__buttons"
-                        }
-                      >
-                        <label className="sort__label" htmlFor="sort">
-                          Sort by
-                        </label>
-                        <select
-                          id="sort"
-                          className="select white"
-                          onChange={(e) =>
-                            sortNode(
-                              e.target.value,
-                              sortModeOrderList[i].direction,
-                              i,
-                            )
-                          }
-                          value={sortModeOrderList[i].mode}
-                        >
-                          <option value="name">Name</option>
-                          <option value="category">Category</option>
-                          {i !== 0 && (
-                            <>
-                              <option value="sourceCount">Source count</option>
-                              <option value="targetCount">Target count</option>
-                            </>
-                          )}
-                        </select>
-                        <div className="sort">
-                          <button
-                            onClick={() =>
-                              sortNode(
-                                sortModeOrderList[i].mode,
-                                sortModeOrderList[i].direction === "up"
-                                  ? "down"
-                                  : "up",
-                                i,
-                              )
-                            }
-                            className={`sort__button`}
-                          >
-                            <div
-                              className={`sort__button up ${
-                                sortModeOrderList[i].direction === "up"
-                                  ? "active"
-                                  : ""
-                              }`}
-                            />
-                            <div
-                              className={`sort__button down ${
-                                sortModeOrderList[i].direction === "down"
-                                  ? "active"
-                                  : ""
-                              }`}
-                            />
-                          </button>
-                        </div>
-                      </div>
-
-                      <ul
-                        className={
-                          i === 0 ? "result_list first" : "result_list"
-                        }
-                      >
-                        {nodes.map((v, j) => (
-                          <li key={j} className="result_list__item">
-                            {i !== 0 && (
-                              <p
-                                id={`label${i}-${v.name}`}
-                                className="label_list label_list__item label_list__item__inner"
-                              >
-                                {v.link}
-                              </p>
-                            )}
-                            <ExploreResultItem
-                              i={i}
-                              v={v}
-                              route={props.route}
-                              ids={props.ids}
-                              selectDatabase={selectDatabase}
-                            />
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
+                  {props.databaseNodesList.map((nodes, i) => (
+                    <ExploreResult
+                      key={i}
+                      i={i}
+                      nodes={nodes}
+                      route={props.route}
+                      ids={props.ids}
+                      selectDatabase={selectDatabase}
+                    />
                   ))}
                 </div>
               </ArrowArea>
---
+++ src/components/ExploreResult.tsx
@@ -0,0 +1,170 @@
+import React from "react";
+import ExploreResultItem from "@/components/ExploreResultItem";
+
+const sortConfig = {
+  name: { up: "desc", down: "asc" },
+  category: { up: "desc", down: "asc" },
+  sourceCount: { up: "asc", down: "desc" },
+  targetCount: { up: "asc", down: "desc" },
+} as const;
+
+const ExploreResult = (props: any) => {
+  const { datasetConfig } = useConfig();
+
+  const sortFunc = (
+    item: any[],
+    mode: keyof typeof sortConfig,
+    direction: "up" | "down",
+  ) => {
+    const n = sortConfig[mode][direction] === "asc" ? 1 : -1;
+
+    if (mode === "name") {
+      return item.toSorted((a, b) => {
+        if (
+          datasetConfig[a.name].label.toLowerCase() <
+          datasetConfig[b.name].label.toLowerCase()
+        ) {
+          return n * -1;
+        } else if (
+          datasetConfig[a.name].label.toLowerCase() >
+          datasetConfig[b.name].label.toLowerCase()
+        ) {
+          return n * 1;
+        }
+
+        return 0;
+      });
+    } else if (mode === "category") {
+      return item.toSorted((a, b) => {
+        if (a.category < b.category) {
+          return n * -1;
+        } else if (a.category > b.category) {
+          return n * 1;
+        }
+        return 0;
+      });
+    } else if (mode === "sourceCount") {
+      return item.toSorted((a, b) => (a.source - b.source) * n);
+    } else if (mode === "targetCount") {
+      return item.toSorted((a, b) => (a.target - b.target) * n);
+    }
+
+    return item;
+  };
+
+  // defaultではnameとascでソートする
+  const [nodesList, setNodesList] = useState(
+    sortFunc(props.nodes, "name", "down"),
+  );
+  const [sortModeOrder, setSortModeOrder] = useState<{
+    mode: keyof typeof sortConfig;
+    direction: "up" | "down";
+  }>({
+    mode: "name",
+    direction: "down",
+  });
+
+  useEffect(() => {
+    // 直前の状態と比較して変化していればnameとascでソートする
+    setNodesList(sortFunc(props.nodes, "name", "down"));
+    setSortModeOrder({
+      mode: "name",
+      direction: "down",
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.nodes]);
+
+  const sortNode = (
+    mode: keyof typeof sortConfig,
+    direction: "up" | "down",
+  ) => {
+    setNodesList(sortFunc(nodesList, mode, direction));
+
+    setSortModeOrder({
+      mode: mode,
+      direction: direction,
+    });
+  };
+
+  return (
+    <div className="item_wrapper">
+      <div
+        className={
+          props.i === 0
+            ? "item_wrapper__buttons first"
+            : "item_wrapper__buttons"
+        }
+      >
+        <label className="sort__label" htmlFor="sort">
+          Sort by
+        </label>
+        <select
+          id="sort"
+          className="select white"
+          onChange={(e) =>
+            sortNode(
+              e.target.value as keyof typeof sortConfig,
+              sortModeOrder.direction,
+            )
+          }
+          value={sortModeOrder.mode}
+        >
+          <option value="name">Name</option>
+          <option value="category">Category</option>
+          {props.i !== 0 && (
+            <>
+              <option value="sourceCount">Source count</option>
+              <option value="targetCount">Target count</option>
+            </>
+          )}
+        </select>
+        <div className="sort">
+          <button
+            onClick={() =>
+              sortNode(
+                sortModeOrder.mode,
+                sortModeOrder.direction === "up" ? "down" : "up",
+              )
+            }
+            className={`sort__button`}
+          >
+            <div
+              className={`sort__button up ${
+                sortModeOrder.direction === "up" ? "active" : ""
+              }`}
+            />
+            <div
+              className={`sort__button down ${
+                sortModeOrder.direction === "down" ? "active" : ""
+              }`}
+            />
+          </button>
+        </div>
+      </div>
+
+      <ul className={props.i === 0 ? "result_list first" : "result_list"}>
+        {nodesList.map((v, j) => (
+          <li key={j} className="result_list__item">
+            {props.i !== 0 && (
+              <p
+                id={`label${props.i}-${v.name}`}
+                className="label_list label_list__item label_list__item__inner"
+              >
+                {v.link}
+              </p>
+            )}
+            <ExploreResultItem
+              i={props.i}
+              v={v}
+              route={props.route}
+              ids={props.ids}
+              selectDatabase={props.selectDatabase}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ExploreResult;
