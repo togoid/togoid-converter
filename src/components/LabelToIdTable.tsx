2ab33366f5c1c999202a95d3900b845942a7befd
akazawa <akazawa@info-lounge.jp>
2024-03-26 17:16:04 +0900

    Show symbol

--- src/components/LabelToIdTable.tsx
+++ src/components/LabelToIdTable.tsx
@@ -15,11 +15,42 @@ const LabelToIdTable = ({ pubdictionariesParam, dataset }: Props) => {
   const { data: tableData } = useSWRImmutable(
     pubdictionariesParam,
     async (key) => {
-      const res = await axios.get("https://pubdictionaries.org/find_ids.json", {
-        params: key,
-      });
+      const res = await axios.get<any>(
+        "https://pubdictionaries.org/find_ids.json",
+        {
+          params: key,
+        },
+      );
 
-      return res.data[pubdictionariesParam.labels] as any[];
+      const tableBaseData = res.data[pubdictionariesParam.labels] as any[];
+
+      const preferredDictionary = dataset.label_resolver.dictionaries.find(
+        (v: any) => v.preferred,
+      )?.dictionary;
+
+      const synonymIdList = tableBaseData
+        .filter((v: any) => v.dictionary !== preferredDictionary)
+        .map((v) => v.identifier);
+
+      if (synonymIdList.length) {
+        const res2 = await axios.get<any>(
+          "https://pubdictionaries.org/find_terms.json",
+          {
+            params: {
+              ids: synonymIdList.join("|"),
+              dictionaries: preferredDictionary,
+            },
+          },
+        );
+
+        tableBaseData.forEach((v) => {
+          if (res2.data[v.identifier] && v.dictionary !== preferredDictionary) {
+            v.symbol = res2.data[v.identifier][0].label;
+          }
+        });
+      }
+
+      return tableBaseData;
     },
   );
 
@@ -47,7 +78,7 @@ const LabelToIdTable = ({ pubdictionariesParam, dataset }: Props) => {
                     )?.label
                   }
                 </th>
-                <th></th>
+                <th>{v.symbol}</th>
                 <th>{v.score}</th>
                 <th>{v.identifier}</th>
               </tr>
