122f549fffa7d6cb69c9faf58b7ee14ed57bdfb0
akazawa <akazawa@info-lounge.jp>
2024-03-28 15:42:09 +0900

    copy id in input area

--- src/components/LabelToIdTable.tsx
+++ src/components/LabelToIdTable.tsx
@@ -13,6 +13,8 @@ type Props = {
 };
 
 const LabelToIdTable = ({ pubdictionariesParam, dataset }: Props) => {
+  const setText = useSetAtom(textAtom);
+
   const { data: tableData } = useSWRImmutable(
     pubdictionariesParam,
     async (key) => {
@@ -55,37 +57,50 @@ const LabelToIdTable = ({ pubdictionariesParam, dataset }: Props) => {
     },
   );
 
+  const inputResultId = () => {
+    if (!tableData) {
+      return;
+    }
+
+    setText(tableData.map((v) => v.identifier).join("\n"));
+  };
+
   return (
     <div>
       {tableData && (
-        <table>
-          <thead>
-            <tr>
-              <th>Input</th>
-              <th>Match type</th>
-              <th>Symbol</th>
-              <th>Score</th>
-              <th>ID</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData.map((v, i) => (
-              <tr key={i}>
-                <th>{v.label}</th>
-                <th>
-                  {
-                    dataset.label_resolver.dictionaries.find(
-                      (w: any) => w.dictionary === v.dictionary,
-                    )?.label
-                  }
-                </th>
-                <th>{v.symbol}</th>
-                <th>{v.score}</th>
-                <th>{v.identifier}</th>
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>Input</th>
+                <th>Match type</th>
+                <th>Symbol</th>
+                <th>Score</th>
+                <th>ID</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {tableData.map((v, i) => (
+                <tr key={i}>
+                  <th>{v.label}</th>
+                  <th>
+                    {
+                      dataset.label_resolver.dictionaries.find(
+                        (w: any) => w.dictionary === v.dictionary,
+                      )?.label
+                    }
+                  </th>
+                  <th>{v.symbol}</th>
+                  <th>{v.score}</th>
+                  <th>{v.identifier}</th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div>
+            <button onClick={() => inputResultId()}>Put id</button>
+          </div>
+        </>
       )}
     </div>
   );
