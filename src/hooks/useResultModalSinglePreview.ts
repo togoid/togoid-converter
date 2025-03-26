cc43550ac5387b0e8471230a68c270293ca5394a
akazawa <akazawa@info-lounge.jp>
2025-03-26 15:46:47 +0900

    annotate target checkbox single

--- src/hooks/useResultModalSinglePreview.ts
+++ src/hooks/useResultModalSinglePreview.ts
@@ -1,20 +1,7 @@
-const useResultModalSinglePreview = (
-  route: Route[],
-  tableHead: (DatasetConfig[number] & {
-    index: number;
-    name: string;
-  })[],
-) => {
+const useResultModalSinglePreview = (route: Route[]) => {
   const filterTable = useMemo(() => {
     return {
-      heading: tableHead,
-      rows: tableHead[0].format?.length
-        ? route[0].results.map((v) => [
-            tableHead[0].format!.reduce((prev, curr) => {
-              return prev.replace(curr.replace("%s", ""), "");
-            }, v),
-          ])
-        : route[0].results.map((v) => [v]),
+      rows: route[0].results.map((v) => [v]),
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
